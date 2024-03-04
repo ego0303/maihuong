8048af33ae65deb934813f84db3aad3aa094a204
nniini <97218937+FouIII@users.noreply.github.com>
2024-03-04 21:05:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Đó là những gì một cô géi ngoan nên làm:))',
     messDesc: 'Mai mua cho ly sữa:))',
     btnAccept: 'Ok luôn nà',
-    messLink: 'https://www.youtube.com/watch?v=ArmDp-zijuc' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.youtube.com/watch?v=pShSyUm-ixg' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
