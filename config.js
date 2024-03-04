77f015ae89a456331359e017102a9de31f94491a
nniini <97218937+FouIII@users.noreply.github.com>
2024-03-04 20:59:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Thảo Vân",
-    introTitle: 'Thảo Vân',
-    introDesc: `Ngay khi mấy người nhìn thấy dòng chữ vô tri này, Vũ trụ đang nói với tui rằng hãy nhắc người đẹp đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
+    titleWeb: "dxx.kaitlyn",
+    introTitle: 'Khánh Linh',
+    introDesc: `Ngay khi người đẹp nhìn thấy dòng chữ vô tri này, Vũ trụ đang nói với tui rằng hãy nhắc người đẹp đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
     btnIntro: 'Tiếp nào',
-    title: 'heyy heyy người đẹp, mấy người đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
+    title: 'heyy heyy người đẹp, cậu đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
     desc: 'Nếu đọc được đến dòng này thì đi ngủ đi, hết văn chương để viết ời :))',
     btnYes: 'Đi ngủ',
     btnNo: 'Hỏng ngủ))',
-    question: 'Tui biết mấy người bất lực lắm mới bấm nút đi ngủ chứ gì, biết hết!!😏',
+    question: 'Tui biết mấy  bất lực lắm mới bấm nút đi ngủ chứ gì, biết hết!!😏',
     btnReply: 'Gửi',
     reply: 'Đi ngủ đi ngủ không ngủ cũng tắt điện thoại mà lên giường nằm😌😌',
     mess: 'Đó là những gì một cô géi ngoan nên làm:))',
     messDesc: 'Mai mua cho ly sữa:))',
     btnAccept: 'Ok luôn nà',
-    messLink: 'https://www.youtube.com/watch?v=9TSPbfbJUkQ' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.youtube.com/watch?v=ArmDp-zijuc' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
