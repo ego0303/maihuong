4f385694c0427b5bae81e46ca117a48c6d30f181
ego <97218937+ego0303@users.noreply.github.com>
2024-07-29 23:22:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Bảo Uyên",
-    introTitle: 'Bảo Uyên',
+    titleWeb: "Hạnh Tiên",
+    introTitle: 'Hạnh Tiên',
     introDesc: `Ngay khi người đẹp nhìn thấy dòng chữ vô tri này, Vũ trụ đang nói với tui rằng hãy nhắc người đẹp đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
     btnIntro: 'Tiếp nào',
     title: 'heyy heyy người đẹp, em đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Đó là những gì một cô géi ngoan nên làm:))',
     messDesc: 'Mai mua cho ly sữa:))',
     btnAccept: 'Ok luôn nà',
-    messLink: 'https://www.youtube.com/watch?v=9TSPbfbJUkQ' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://youtu.be/KwZvECZFmtE' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
