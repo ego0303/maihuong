9f28cf473bf42dabf841a8389d5d6e98a02f1cfb
ego <97218937+ego0303@users.noreply.github.com>
2024-08-18 23:54:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Đó là những gì một cô géi ngoan nên làm:))',
     messDesc: 'Mai mua cho ly sữa:))',
     btnAccept: 'Ok luôn nà',
-    messLink: 'https://youtu.be/NTpbbQUBbuo' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://youtu.be/8Zrdgd5wrCc' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
