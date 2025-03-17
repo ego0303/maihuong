f0dfcd34ed22d9dc29d0257f9e6124e01b8072cb
ego <97218937+ego0303@users.noreply.github.com>
2025-03-18 01:46:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    titleWeb: "Mai Hương",
-    introTitle: 'Mochi',
+    titleWeb: "Thảo Nhi",
+    introTitle: 'Thảo Nhi',
     introDesc: `Ngay khi người đẹp nhìn thấy dòng chữ vô tri này, Vũ trụ đang nói với tui rằng hãy nhắc người đẹp đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
-    title: 'heyy heyy cô gái , cậu đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
+    title: 'heyy heyy cô gái , em đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
     desc: 'Nếu đọc được đến dòng này thì đi ngủ đi, tui hết văn chương để viết ời :))',
     btnYes: 'Đi ngủ',
     btnNo: 'Hỏng ngủ))',
